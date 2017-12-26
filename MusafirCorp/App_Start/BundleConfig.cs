7a163345263d09c8d12ed5eccaae0967b98c97f9
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-12-26 08:55:04 +0530

    Completely remove Bootstrap

--- MusafirCorp/App_Start/BundleConfig.cs
+++ MusafirCorp/App_Start/BundleConfig.cs
@@ -19,17 +19,17 @@ namespace MusafirCorp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
                 "~/Scripts/MJs/materialize.js"
                 ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/css/Mtr").Include(
                 "~/Content/MStyle/materialize.css"
